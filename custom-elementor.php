3fb232a3670b0f0508e9f59f717932d8bdcccb72
Lornz18 <mellgodwinbarza22@gmail.com>
2024-09-05 14:21:09 +0800

    Dabest

---
+++ custom-elementor.php
@@ -0,0 +1,17 @@
+<?php 
+/** 
+ * Template Name: Custom Elementor
+ * 
+ * 
+ * 
+ */ 
+?>
+
+
+<?php get_header()?>
+
+<?php if ( ! function_exists( 'elementor_theme_do_location' ) || ! elementor_theme_do_location( 'page' ) ) {
+    the_content();
+} ?>
+
+<?php get_footer()?>
